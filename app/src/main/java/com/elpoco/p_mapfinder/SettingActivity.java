24d1a811c8bdb5da64d75c1b755e74488d3d0d38
Elpoco <wjdwlgus96@naver.com>
2019-02-13 15:32:17 +0900

    master

--- app/src/main/java/com/elpoco/p_mapfinder/SettingActivity.java
+++ app/src/main/java/com/elpoco/p_mapfinder/SettingActivity.java
@@ -2,6 +2,7 @@ package com.elpoco.p_mapfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+    }
+
+    public void clickLogo(View view) {
+        finish();
     }
 }
